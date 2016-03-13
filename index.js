5761b035f9144035233a0511418a62352ac9b6eb
cnweibo <matiascx@163.com>
2016-03-14 00:01:50 +0800

    draft hover of course

--- index.js
+++ index.js
@@ -6,4 +6,25 @@
     search.addEventListener('mouseleave',function(ev){
         search.setAttribute('src','images/search.png');
     });
+    var course = document.getElementsByClassName('course col1-4');
+    for (var i = 0; i < course.length; i++) {
+    	course[i].addEventListener('mouseenter', function(e) {
+    		var that = this;
+    		var detail = that.querySelectorAll('div.detail')[0];
+    		if (detail.className.indexOf('j-hidden')) {
+	    		detail.classList.remove('j-hidden');
+                detail.classList.add('j-coursehovered');
+	    		// detail.className +=' j-coursehovered';
+	    	}
+    	});
+		course[i].addEventListener('mouseleave', function(e) {
+			var that = this;
+			var detail = that.querySelectorAll('div.detail')[0];
+			if (detail.className.indexOf('j-hidden')==-1) {
+	    		detail.classList.add('j-hidden');
+                detail.classList.remove('j-coursehovered');
+	    		// detail.className +=' j-coursehovered';
+	    	}
+		});
+    }
 }())
