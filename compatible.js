ec58d7bc311dcd92710c35926183593b80cbd0ca
cnweibo <matiascx@163.com>
2016-03-23 00:21:38 +0800

    getComputedStyle compatible for IE8

--- compatible.js
+++ compatible.js
@@ -133,19 +133,27 @@
     } 
 
 // getComputedStyle for IE8
-// getComputedStyle
-!('getComputedStyle' in this) && (this.getComputedStyle = (function () {
-    function getPixelSize(element, style, property, fontSize) {
+(function (global) {
+    if (window.getComputedStyle){return;}
+    function getComputedStylePixel(element, property, fontSize) {
         var
-        sizeWithSuffix = style[property],
-        size = parseFloat(sizeWithSuffix),
-        suffix = sizeWithSuffix.split(/\d/)[0],
+        // Internet Explorer sometimes struggles to read currentStyle until the element's document is accessed.
+        value = element.document && element.currentStyle[property].match(/([\d\.]+)(%|cm|em|in|mm|pc|pt|)/) || [0, 0, ''],
+        size = value[1],
+        suffix = value[2],
         rootSize;
 
-        fontSize = fontSize != null ? fontSize : /%|em/.test(suffix) && element.parentElement ? getPixelSize(element.parentElement, element.parentElement.currentStyle, 'fontSize', null) : 16;
+        fontSize = !fontSize ? fontSize : /%|em/.test(suffix) && element.parentElement ? getComputedStylePixel(element.parentElement, 'fontSize', null) : 16;
         rootSize = property == 'fontSize' ? fontSize : /width/i.test(property) ? element.clientWidth : element.clientHeight;
 
-        return (suffix == 'em') ? size * fontSize : (suffix == 'in') ? size * 96 : (suffix == 'pt') ? size * 96 / 72 : (suffix == '%') ? size / 100 * rootSize : size;
+        return suffix == '%' ? size / 100 * rootSize :
+               suffix == 'cm' ? size * 0.3937 * 96 :
+               suffix == 'em' ? size * fontSize :
+               suffix == 'in' ? size * 96 :
+               suffix == 'mm' ? size * 0.3937 * 96 / 10 :
+               suffix == 'pc' ? size * 12 * 96 / 72 :
+               suffix == 'pt' ? size * 96 / 72 :
+               size;
     }
 
     function setShortStyleProperty(style, property) {
@@ -156,23 +164,44 @@
         b = property + 'Bottom' + borderSuffix,
         l = property + 'Left' + borderSuffix;
 
-        style[property] = (style[t] == style[r] == style[b] == style[l] ? [style[t]]
-        : style[t] == style[b] && style[l] == style[r] ? [style[t], style[r]]
-        : style[l] == style[r] ? [style[t], style[r], style[b]]
-        : [style[t], style[r], style[b], style[l]]).join(' ');
+        style[property] = (style[t] == style[r] && style[t] == style[b] && style[t] == style[l] ? [ style[t] ] :
+                           style[t] == style[b] && style[l] == style[r] ? [ style[t], style[r] ] :
+                           style[l] == style[r] ? [ style[t], style[r], style[b] ] :
+                           [ style[t], style[r], style[b], style[l] ]).join(' ');
     }
 
+    // <CSSStyleDeclaration>
     function CSSStyleDeclaration(element) {
         var
-        currentStyle = element.currentStyle,
         style = this,
-        fontSize = getPixelSize(element, currentStyle, 'fontSize', null);
+        currentStyle = element.currentStyle,
+        fontSize = getComputedStylePixel(element, 'fontSize'),
+        unCamelCase = function (match) {
+            return '-' + match.toLowerCase();
+        },
+        property;
 
         for (property in currentStyle) {
-            if (/width|height|margin.|padding.|border.+W/.test(property) && style[property] !== 'auto') {
-                style[property] = getPixelSize(element, currentStyle, property, fontSize) + 'px';
-            } else if (property === 'styleFloat') {
-                style['float'] = currentStyle[property];
+            Array.prototype.push.call(style, property == 'styleFloat' ? 'float' : property.replace(/[A-Z]/, unCamelCase));
+
+            if (property == 'width') {
+                style[property] = element.offsetWidth + 'px';
+            } else if (property == 'height') {
+                style[property] = element.offsetHeight + 'px';
+            } else if (property == 'styleFloat') {
+                style.float = currentStyle[property];
+            } else if (/margin.|padding.|border.+W/.test(property) && style[property] != 'auto') {
+                style[property] = Math.round(getComputedStylePixel(element, property, fontSize)) + 'px';
+            } else if (/^outline/.test(property)) {
+                // errors on checking outline
+                try {
+                    style[property] = currentStyle[property];
+                } catch (error) {
+                    style.outlineColor = currentStyle.color;
+                    style.outlineStyle = style.outlineStyle || 'none';
+                    style.outlineWidth = style.outlineWidth || '0px';
+                    style.outline = [style.outlineColor, style.outlineWidth, style.outlineStyle].join(' ');
+                }
             } else {
                 style[property] = currentStyle[property];
             }
@@ -182,26 +211,42 @@
         setShortStyleProperty(style, 'padding');
         setShortStyleProperty(style, 'border');
 
-        style.fontSize = fontSize + 'px';
-
-        return style;
+        style.fontSize = Math.round(fontSize) + 'px';
     }
 
     CSSStyleDeclaration.prototype = {
         constructor: CSSStyleDeclaration,
-        getPropertyPriority: function () {},
-        getPropertyValue: function ( prop ) {
-            return this[prop] || '';
+        // <CSSStyleDeclaration>.getPropertyPriority
+        getPropertyPriority: function () {
+            throw new Error('NotSupportedError: DOM Exception 9');
         },
-        item: function () {},
-        removeProperty: function () {},
-        setProperty: function () {},
-        getPropertyCSSValue: function () {}
+        // <CSSStyleDeclaration>.getPropertyValue
+        getPropertyValue: function (property) {
+            return this[property.replace(/-\w/g, function (match) {
+                return match[1].toUpperCase();
+            })];
+        },
+        // <CSSStyleDeclaration>.item
+        item: function (index) {
+            return this[index];
+        },
+        // <CSSStyleDeclaration>.removeProperty
+        removeProperty: function () {
+            throw new Error('NoModificationAllowedError: DOM Exception 7');
+        },
+        // <CSSStyleDeclaration>.setProperty
+        setProperty: function () {
+            throw new Error('NoModificationAllowedError: DOM Exception 7');
+        },
+        // <CSSStyleDeclaration>.getPropertyCSSValue
+        getPropertyCSSValue: function () {
+            throw new Error('NotSupportedError: DOM Exception 9');
+        }
     };
 
-    function getComputedStyle(element) {
+    // <Global>.getComputedStyle
+    
+    global.getComputedStyle = function getComputedStyle(element) {
         return new CSSStyleDeclaration(element);
-    }
-
-    return getComputedStyle;
-})(this));
+    };
+})(this);
