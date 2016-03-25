cda102f36cd01392a5284d9499be61226d0ef2c3
cnweibo <matiascx@163.com>
2016-03-25 20:43:15 +0800

    tab inactive class

--- coursetabs.js
+++ coursetabs.js
@@ -31,7 +31,7 @@
                 that.pagination = data.pagination;
                 var _tabs = '<div class="tab">\ ';
                 for (var i = 0; i < that.tabs.length; i++) {//默认第0个tab为active //IE8奇怪的是length为3包含一个proto
-                    _tabs += '<div class="tabitem '+(i==that._activetab?"active":"")+'">'+that.tabs[i].title+'</div>\ '
+                    _tabs += '<div class="tabitem '+(i==that._activetab?"active":"inactive")+'">'+that.tabs[i].title+'</div>\ '
                 };
                 _tabs+='</div>\ ';
                 var tabcontainer ='<div class="maincontent m-tabs">\
