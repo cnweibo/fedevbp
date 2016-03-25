380514f48d8f3ecc2db72ce9b9ed19a30954ffa0
cnweibo <matiascx@163.com>
2016-03-25 20:43:58 +0800

    redesign hotcourse animation

--- animate.js
+++ animate.js
@@ -12,25 +12,15 @@ var animateFunc = function() {
 		    hotcourseRemainingNodes.push(item);
 		};
     var firsttocheck = getElementsByClassName('m-hostcourseitem')[0];
-    var heightParent = window.getComputedStyle(firsttocheck.parentNode);
+    var heightParent = window.getComputedStyle(firsttocheck.parentNode).height;
     firsttocheck.parentNode.style.cssText.height = heightParent;
 	var timer = window.setInterval(function() {
-
-            var first = getElementsByClassName('m-hostcourseitem')[0];
-            var heightParent = parseInt(window.getComputedStyle(first.parentNode).height);
-            var margintop = window.getComputedStyle(first).marginTop;
-            var done = function(){
-                return function(){
-                    first.parentNode.removeChild(first);
-                    first.style.marginTop = margintop;
-                    getElementsByClassName('m-hostcourseitem')[0].parentNode.appendChild(hotcourseRemainingNodes.pop());
-                    //first.style.cssText = "margin-top: "+ margintop ;
-
-                    hotcourseRemainingNodes.push(first);
-                }
-            }()
             var height = parseInt(window.getComputedStyle(getElementsByClassName('m-hostcourseitem')[0]).height);
+            // 
+            // 1.启动10ms timer调用stepHotCourse函数实现first元素动画
+            var first = getElementsByClassName('m-hostcourseitem')[0];
             var n =0;
+            window.setTimeout(stepHotCourse, 10); //50 times
             function stepHotCourse() {
                 n++;
                 first.style.marginTop = (-n*height/50)+'px';
@@ -40,11 +30,20 @@ var animateFunc = function() {
                     window.setTimeout(stepHotCourse, 10);
                 }
             }
-            window.setTimeout(stepHotCourse, 10); //50 times
-
-            // $(first).animate({
-            //     marginTop: -1*parseInt(window.getComputedStyle( $('.hotlist .m-hostcourseitem:first')[0]).height)
-            // }, 500,done);
-	},2000);
+            // 2.动画结束将first删除，数据归入remaining尾部，将remaining中的头元素创建dom插入active队列尾部
+            
+            var done = function() {
+                
+                hotcourseRemainingItems.push(hotlistdata.shift());
+                var newdatatoinsert = hotcourseRemainingItems.pop();
+                hotlistdata.push(newdatatoinsert);
+                var item = document.createElement('div');
+                item.setAttribute('class','m-hostcourseitem');
+                var innerhtml = ' <img src="'+newdatatoinsert.middlePhotoUrl+'" alt=""><div class="info"><h4>'+newdatatoinsert.name+'</h4><div class="studentnum">'+newdatatoinsert.learnerCount+'</div></div>';
+                item.innerHTML = innerhtml;
+                first.parentNode.appendChild(item);
+                first.parentNode.removeChild(first);
+            }
+	},5000);
 
 }
