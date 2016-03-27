cdb21b13a5c46416431fba20e3080d75eabdca0d
cnweibo <matiascx@163.com>
2016-03-27 19:33:09 +0800

    style.css and style.css.map to be added

--- archive.sh
+++ archive.sh
@@ -1,1 +1,8 @@
-git archive --format=zip -o archived.zip HEAD
+mkdir ../archive
+git archive master | tar -x -C  ../archive
+git checkout gh-pages
+cp -R -f ../archive .
+git add .
+git commit -m"arhive gh-page"
+git push
+rm -rf ../archive
