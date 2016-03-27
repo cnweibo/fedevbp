30e938e154e6f2dbbbbfdf1e7721f1701986a7a1
cnweibo <matiascx@163.com>
2016-03-27 20:07:34 +0800

    archive script update

--- archive.sh
+++ archive.sh
@@ -1,7 +1,7 @@
 mkdir ../archive
 git archive master | tar -x -C  ../archive
 git checkout gh-pages
-cp -R -f ../archive .
+cp -R -f ../archive/* .
 git add .
 git commit -m"arhive gh-page"
 git push
